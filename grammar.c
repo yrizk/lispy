c84c2348ce3633066efa6a1f19350e2838aa7d14
Yohan Rizk <yrizk@google.com>
2018-11-14 10:49:31 -0500

    added grammar code

---
+++ grammar.c
@@ -0,0 +1,21 @@
+// an experimentation with grammars
+#include "mpc.h"
+
+int main(int argc, char** argv) {
+
+  mpc_parser_t* Adjective = mpc_or(4,
+      mpc_sym("wow"), mpc_sym("many"),
+      mpc_sym("so"), mpc_sym("such")
+  );
+
+  mpc_parser_t* Noun = mpc_or(4,
+      mpc_sym("lispy"), mpc_sym("language"),
+      mpc_sym("book"), mpc_sym("build"), mpc_sym("c")
+  );
+
+  mpc_parser_t* Phrase = mpc_and(2, mpcf_strfold, Adjective, Noun, free);
+  mpc_parser_t* Doge = mpc_many(mpcf_strfold, Phrase);
+  // do some parsing here.
+  mpc_delete(Doge);
+  return 0;
+}
